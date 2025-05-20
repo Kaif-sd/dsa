00e5b0c167e947c8c3e7b5dad97c6d29273ba2f1
kaif momin <mominkaif858@gmail.com>
2025-05-20 22:18:43 +0530

    Reverse String Using Recursion

---
+++ Arrays/Reverse_String_Using_Recursion/ReverseStringUsingRecursion.java
@@ -0,0 +1,25 @@
+package Arrays.Reverse_String_Using_Recursion;
+
+public class ReverseStringUsingRecursion {
+    
+    public static char []reverseString(char charArray [] , int left , int right){
+        if(charArray == null || left >= right){
+            return charArray;
+        }
+
+        char temp = charArray[left];
+        charArray[left] = charArray[right];
+        charArray[right] = temp;
+
+        return reverseString(charArray, ++left, --right);
+    }
+
+    public static void main(String[] args) {
+        String string = "This is String for reversing using recursion";
+        char charArray [] = string.toCharArray();
+        int stringLength = string.length();
+        charArray = reverseString(charArray , 0 , stringLength-1);
+        string = new String(charArray);
+        System.out.println(string);
+    }
+}
