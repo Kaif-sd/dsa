756bd679a9a559d474393fa25a3121c1f3e3de25
kaif momin <mominkaif858@gmail.com>
2025-05-20 22:07:08 +0530

    Reverse String Using Stack

---
+++ Arrays/Reverse_String_Using_Stack/ReverseStringUsingStack.java
@@ -0,0 +1,26 @@
+package Arrays.Reverse_String_Using_Stack;
+
+import java.util.Stack;
+
+public class ReverseStringUsingStack {
+
+    public static String reverseString(String string){
+        Stack<Character> stack = new Stack<>();
+        for(int i=0 ; i<string.length(); i++){
+            stack.push(string.charAt(i));
+        }
+
+        string = "";
+        while (!stack.isEmpty()) {
+            char popedChar = stack.pop();
+            string += popedChar;
+        }
+        return string;
+    }
+
+    public static void main(String[] args) {
+        String string = "This is my String";
+        string = reverseString(string);
+        System.out.println(string);
+    }
+}
